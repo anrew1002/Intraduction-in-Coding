2c15a4b2ee633fc4a9e014a40f1a1ebb516a50cb
anrew1002 <you@example.com>
2021-11-05 02:06:21 +0800

    Sort ver2.

--- Sort.py
+++ Sort.py
@@ -1,57 +1,101 @@
 
-def sort(a=[1,2,7,8,9,10],b=[3,6]):
-    c=[]
-    a_num=0
-    b_num=0
-    length_a=len(a)
-    length_b=len(b)
-    if a[0] < b[0]:
-        c.append(a[0])
-        a_num=1
-        length_a-=1
-        print(length_a)
+#print(int(3.5))
+def mas_division(Base):
+    if len(Base)<2:
+        return Base
     else:
-        b_num=1
-        c.append(b[0])
-        length_b-=1
-    print('first Iter',c,length_a,length_b)
-    
-    for i in range(a_num,length_a):
-        flag_of_appending=False
-        print('a[i]',a[i])
-        for j in range(b_num,length_b):
-            print('b[j]=',b[j])
-            if a[i] < b[j]:
-                flag_of_appending=True
-                c.append(a[i])
-                print('*',c)
-                break
-            else:
-                c.append(b[j])
-                print('-',c)
-    tail=0
-    if flag_of_appending==False:
-        c.append(a[i])
-        i+=1
-        print('a=',a[i])
-        tail=length_a-length_b
-        while not tail ==0:
-            print('tail_a',tail)
-            c.append(a[i])
+        i=int(len(Base)/2)
+        left=mas_division(Base[:i])
+       # print(left)
+        right=mas_division(Base[i:])
+        #print(right)
+        return compare(left,right)
+def compare(left,right):
+    sortedMAS=[]
+    i=0
+    j=0
+    #print(left,right,"----")
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            sortedMAS.append(left[i])
+            #print("left:",sortedMAS)
             i+=1
-            tail-=1
-    else:
-        tail=length_b-length_a
-        while not tail ==0:
-            print('tail_b',tail)
-            c.append(a[j])
+            #print(i)
+        #if right[i]<left[i]:
+        else:
+            sortedMAS.append(right[j])
+            #print("right",sortedMAS)
             j+=1
-            tail-=1
-    print('tail',tail)
-    print(c)
-sort()
+    while i<len(left):
+        sortedMAS.append(left[i])
+        i+=1
+    while j<len(right):
+        sortedMAS.append(right[j])
+        j+=1
+    #print("sort:",sortedMAS)
+    return sortedMAS
+def CreateNums():
+    import random
+    file=open("nums.txt","w")
+    a=[x for x in range(300)]
+    random.shuffle(a)
+    for l in a:
+        file.write(str(l))
+        file.write("\n") 
+    
+#base=[1,4,3,5,2,8,7]
+#print(base)
+#print(mas_division(base))
 
 
+while True:
+        offer=input("Создать файл nums.txt с рандомно перемешанами числами от 0 до 300? \nРекомендуется если нет заготовленного файла. [yes/no] ")
+        if offer=="yes":
+            CreateNums()
+            break
+        elif offer=="no":
+            break
+        print("\n")
+UserInput=input("\nИмя файла с данными: ")
+Good=False
+while not Good:
+    try:
+        f=open(UserInput)
+    except FileNotFoundError:
+        print("Файл не найден")
+        UserInput=input("Имя файла с данными: ")
+    else:
+        Good=True
         
 
+f=f.readlines()
+massive=[]
+for line in f:
+    massive.append(int(line))
+result=mas_division(massive)
+#print(result)
+try:
+    file=open("results.txt")
+except FileNotFoundError:
+    file=open("results.txt","w")
+    for num in result:
+        file.write(str(num))
+        file.write("\n")
+    file.close()
+else:
+    while True:
+        offer2=input("\nФайл с результатами уже существует. Заменить? [yes/no] ")
+        if offer2=="yes":
+            file=open("results.txt","w+")
+            for num in result:
+                file.write(str(num))
+                file.write("\n")
+            print("Успешно!")
+            break
+        elif offer2=="no":
+            print("Перезапустите программу когда надумаете")
+            break
+    file.close()
+    
+            
     
