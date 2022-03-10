19a945e632e37a8d75823b92d6e5a4a078f4c110
anrew1002 <you@example.com>
2022-03-11 00:20:34 +0800

    игра на питоне работающая

--- 11.03 Python Game with Sprites/at_11.03_pygaming.py
+++ 11.03 Python Game with Sprites/at_11.03_pygaming.py
@@ -1,4 +1,6 @@
 import pygame as pg
+import tkinter as tk
+from tkinter.ttk import *
 from pygame.locals import ( K_UP,
 K_DOWN,
 K_LEFT,
@@ -67,7 +69,13 @@ clock=pg.time.Clock()
 screen.fill((185,177,219))
 
 player1=MyPlayer(300,"player.png")
+enemies=pg.sprite.Group()
 enemy1=Enemy(random.randint(0,SCREEN_HEIGHT),"enemy1.png")
+enemy2=Enemy(random.randint(0,SCREEN_HEIGHT),"enemy2.png")
+enemy3=Enemy(random.randint(0,SCREEN_HEIGHT),"enemy1.png")
+enemies.add(enemy1)
+enemies.add(enemy2)
+enemies.add(enemy3)
 
 running = True
 while running:
@@ -81,15 +89,21 @@ while running:
     screen.fill((185,177,219))
     screen.blit(player1.image,player1.rect)
     screen.blit(enemy1.image,enemy1.rect)
+    screen.blit(enemy2.image,enemy2.rect)
+    screen.blit(enemy3.image,enemy3.rect)
             
     pg.display.flip()
 
     keys=pg.key.get_pressed()
     player1.update(keys)
     enemy1.update()
-    if pg.sprite.collide_rect(player1,enemy1):
+    enemy2.update()
+    enemy3.update()
+    if pg.sprite.spritecollide(player1,enemies,True):
         running=False
-    
+root = tk.Tk()
+label = tk.Label(root, text="Вы проиграли", height=5,width=40)
+label.pack()
     
 
     
