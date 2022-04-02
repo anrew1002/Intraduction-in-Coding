a7918265c6ff835155c2e896bb4c3a6a82c02a84
anrew1002 <91276494+anrew1002@users.noreply.github.com>
2022-04-03 00:23:57 +0800

    Графы, если понраятся, хочу дополнительный балл

---
+++ 1.2/01.04 графы и пути/1.py
@@ -0,0 +1,62 @@
+import copy
+class graph_Matrix():
+    def __init__(self,list1):
+        self.matrix=[]
+        self.naming=list(map(lambda x: x.strip() , list1[0][1:]))
+        for i in range(1,len(list1)):
+            self.matrix.append(list(map(int,a[i][1:])))
+            
+        self.set_path()
+        
+    def __str__(self):
+        return self.output_Matrix(self.matrix)
+    def __repr__(self):
+        return self.output_Matrix(self.matrix)
+    
+    def set_path(self):
+        self.path=[]
+        self.path=copy.deepcopy(self.matrix)
+        counter=0
+        while counter < len(self.path):
+            for i in range(len(self.path)):
+                for j in range(len(self.path[0])):
+                    if (self.path[i][counter]!=0) and (self.path[counter][j]!=0):
+                        if self.path[i][j] > self.path[i][counter]+self.path[counter][j] or self.path[i][j]==0 :
+                            self.path[i][j] = self.path[i][counter]+self.path[counter][j]
+            counter+=1
+        return self.output_Matrix(self.path)
+    
+    def output_Matrix(self,list1):
+        output="  "
+        output+=", ".join(self.naming) +"\n"
+        for i in range(len(list1)):
+            output+= str(self.naming[i])+" "+str(list1[i])[1:-1] + "\n"
+        return output
+    def edit_Matrix(self, param, i,j):
+        self.matrix[i][j]=param
+        self.set_path()
+#основан на алгоритме Флойда + парочка функций для интереса
+def FileReaderMatrix(name):
+  a=[]
+  f=open(name,"r").readlines()
+  for line in f:
+    a.append(list(line.split(" ")))
+  return a
+
+a=[[0, "A","B","C","D"],
+   ["A",0,  1,  6,  0],
+   ["B",0,  0,  4,  1],
+   ["C",0,  0,  0,  0],
+   ["D",0 , 0,  1,  0]]
+a=FileReaderMatrix("input.txt")
+print("\n")
+graph=graph_Matrix(a)
+#graph.edit_Matrix(3,1,0)
+print(graph)
+print(graph.set_path())
+
+
+
+
+
+
