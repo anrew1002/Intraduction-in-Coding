118c98fecf027ed2bbc35223407cda28d1eb0dd8
anrew1002 <91276494+anrew1002@users.noreply.github.com>
2022-04-29 22:28:58 +0800

    Начало Змейки

---
+++ 1.2/pygame ЗМЕЙКА/snake.py
@@ -0,0 +1,125 @@
+from tkinter import messagebox
+import tkinter as tk
+import random
+import math
+import pygame
+
+
+class cube:
+    rows = 20
+    w = 500
+
+    def __init__(self, start, dirnx=1,
+                 dirny=0, color=(255, 0, 0)):
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+
+    def move(self, dirnx, dirny):
+
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+        pygame.draw.rect(surface, self.color, (i * dis +
+                         1, j * dis + 1, dis - 2, dis - 2))
+        if eyes:
+            centre = dis // 2
+            radius = 3
+            circleMiddle = (i * dis + centre - radius,
+                            j * dis + 8)
+            circleMiddle2 = (i * dis + dis - radius *
+                             2, j * dis + 8)
+            pygame.draw.circle(surface, (0, 0, 0),
+                               circleMiddle, radius)
+            pygame.draw.circle(surface, (0, 0, 0),
+                               circleMiddle2, radius)
+
+
+class snake:
+    body = []
+    turns = {}
+
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+
+    def move(self):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        keys = pygame.key.get_pressed()
+        for key in keys:
+            if keys[pygame.K_LEFT]:
+                self.dirnx = -1
+                self.dirny = 0
+                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            elif keys[pygame.K_RIGHT]:
+                self.dirnx = 1
+                self.dirny = 0
+                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            elif keys[pygame.K_UP]:
+                self.dirnx = 0
+                self.dirny = -1
+                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            elif keys[pygame.K_DOWN]:
+                self.dirnx = 0
+                self.dirny = 1
+                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] == c.rows - 1:
+                    c.pos = (0, c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows - 1:
+                    c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] == 0:
+                    continue
+                else:
+                    break
+
+
+pygame.init()
+
+
+def main():
+    global width, rows, s, snack
+    dis = pygame.display.set_mode((400, 300))
+    s = snake((255, 0, 0), (10, 10))
+    snack = cube(10, color=(0, 255, 0))
+    pygame.display.update()
+    clock = pygame.time.Clock()
+    game_over = False
+    while not game_over:
+        pygame.time.delay(50)
+        clock.tick(10)
+        s.move()
+        if s.body[0].pos == snack.pos:
+            # s.addCube()
+            snack = cube(200, color=(0, 255, 0))
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z: z.pos,
+                                         s.body[x + 1:])):
+
+                print('Score: \n', len(s.body))
+
+                s.reset((10, 10))
+    pygame.quit()
+    quit()
+
+
+main()
