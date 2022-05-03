8113dfaeda551fbf215b760dcf70af4788f26e10
anrew1002 <91276494+anrew1002@users.noreply.github.com>
2022-05-03 15:55:08 +0800

    Добавил возможность изменять размер сетки

--- 1.2/15.04_pygame_Snake/snake.py
+++ 1.2/15.04_pygame_Snake/snake.py
@@ -5,25 +5,28 @@ import math
 from turtle import color
 import pygame
 import time
+import copy
 
 pygame.mixer.pre_init(frequency=44100, size=8, channels=2,
                       buffer=512, devicename=None)
 pygame.init()
 pygame.mixer.music.load("YoshidaBrothersNabbed.mp3")
 s_hrum = pygame.mixer.Sound('hrum1.wav')
-s_bum = pygame.mixer.Sound('bum.wav')
+sound_bum = pygame.mixer.Sound('bum.wav')
 
 
 class Snake():
 
-    def __init__(self, pos_start):
+    def __init__(self, pos_start, cell_size, speed):
+        self.size = cell_size
         self.head = pos_start
-        self.body = [[100, 50],
-                     [90, 50],
-                     [80, 50],
-                     [70, 50]
+        pos_start = copy.deepcopy(pos_start)
+        self.body = [pos_start,
+                     [pos_start[0]-(self.size*1), pos_start[1]],
+                     [pos_start[0]-(self.size*2), pos_start[1]],
+                     [pos_start[0]-(self.size*3), pos_start[1]]
                      ]
-        self.speed = 15
+        self.speed = speed
         self.direction = "RIGHT"
         self.change_dir = self.direction
 
@@ -54,18 +57,18 @@ class Snake():
             self.direction = 'RIGHT'
 
         if self.direction == 'UP':
-            self.head[1] -= 10
+            self.head[1] -= self.size
         if self.direction == 'DOWN':
-            self.head[1] += 10
+            self.head[1] += self.size
         if self.direction == 'LEFT':
-            self.head[0] -= 10
+            self.head[0] -= self.size
         if self.direction == 'RIGHT':
-            self.head[0] += 10
+            self.head[0] += self.size
         self.body.insert(0, list(self.head))
         game_window.fill((0, 0, 0))
         for pos in self.body:
             pygame.draw.rect(game_window, (0, 255, 0),
-                             pygame.Rect(pos[0], pos[1], 10, 10))
+                             pygame.Rect(pos[0], pos[1], self.size, self.size))
         for block in self.body[1:]:
             if self.head[0] == block[0] and self.head[1] == block[1]:
                 game_over_window(score, game_window)
@@ -75,18 +78,19 @@ class Snake():
 
 
 class Fruit():
-    def __init__(self,  game_window, is_spawn=True):
+    def __init__(self,  game_window, cell_size, is_spawn=True, ):
         self.window = game_window
         self.spawn = is_spawn
-        self.pos = random_pos()
+        self.pos = random_pos(cell_size)
+        self.size = cell_size
         self.draw()
 
     def draw(self):
         pygame.draw.rect(self.window, (255, 255, 255), pygame.Rect(
-            self.pos[0], self.pos[1], 10, 10))
+            self.pos[0], self.pos[1], self.size, self.size))
 
     def redraw(self):
-        self.pos = random_pos()
+        self.pos = random_pos(self.size)
         self.draw()
 
 
@@ -105,15 +109,15 @@ def game_over_window(score, game_window):
 
 def main(event, root):
 
-    global score, window_x, window_y
+    global score, window_x, window_y, sell_size
     root.destroy()
     game_window = pygame.display.set_mode((window_x, window_y))
     is_game_over = False
     pygame.mixer.music.play(loops=0, start=0.0,
                             fade_ms=0)
     fps = pygame.time.Clock()
-    s = Snake([100, 50])
-    f = Fruit(game_window)
+    s = Snake([100, 50], sell_size, 15)
+    f = Fruit(game_window, sell_size)
 
     while not is_game_over:
         s.move(game_window)
@@ -125,20 +129,20 @@ def main(event, root):
         else:
             s.crawl()
 
-        if s.head[0] < 0 or s.head[0] > window_x-10:
-            s_bum.play()
+        if s.head[0] < 0 or s.head[0] > window_x-sell_size:
+            sound_bum.play()
             game_over_window(score, game_window)
-        if s.head[1] < 0 or s.head[1] > window_y-10:
-            s_bum.play()
+        if s.head[1] < 0 or s.head[1] > window_y-sell_size:
+            sound_bum.play()
             game_over_window(score, game_window)
 
         pygame.display.update()
         fps.tick(s.speed)
 
 
-def random_pos():
-    return [random.randrange(1, (window_x//10)) * 10,
-            random.randrange(1, (window_y//10)) * 10]
+def random_pos(size):
+    return [random.randrange(1, (window_x//size)) * size,
+            random.randrange(1, (window_y//size)) * size]
 
 
 def menu_main():
@@ -217,10 +221,14 @@ def menu_rules(event, w, h, bg):
 
     canvas3.create_text(
         70, 60, anchor="nw", text="Используйте клавиши WASD,\nдля того чтобы съесть яблочки,\nи подрасти.\nНи во что не врезайтесь!", font=("Helvetica", 14, "bold"), fill="white")
+    but_escp = Button(ruler, text="Back", height=1, width=5)
+    but_escp.bind("<Button-1>", lambda e, pr=ruler: pr.destroy())
+    but_ecp_canv = canvas3.create_window(10, 10, anchor="nw", window=but_escp)
 
 
 window_x = 720
 window_y = 480
 score = 0
+sell_size = 5
 
 menu_main()
