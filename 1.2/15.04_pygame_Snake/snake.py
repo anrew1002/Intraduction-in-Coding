bb17291009267523c9cd670c4c7fbc5d72490b6d
anrew1002 <91276494+anrew1002@users.noreply.github.com>
2022-05-04 19:46:57 +0800

    Я всё сломал нужно починить

--- 1.2/15.04_pygame_Snake/snake.py
+++ 1.2/15.04_pygame_Snake/snake.py
@@ -1,10 +1,9 @@
 from email.mime import image
 from tkinter import *
 import random
-import math
 from turtle import color
+from numpy import outer
 import pygame
-import time
 import copy
 
 pygame.mixer.pre_init(frequency=44100, size=8, channels=2,
@@ -13,6 +12,8 @@ pygame.init()
 pygame.mixer.music.load("YoshidaBrothersNabbed.mp3")
 s_hrum = pygame.mixer.Sound('hrum1.wav')
 sound_bum = pygame.mixer.Sound('bum.wav')
+
+# ИГРА:
 
 
 class Snake():
@@ -65,13 +66,43 @@ class Snake():
         if self.direction == 'RIGHT':
             self.head[0] += self.size
         self.body.insert(0, list(self.head))
-        game_window.fill((0, 0, 0))
+
         for pos in self.body:
             pygame.draw.rect(game_window, (0, 255, 0),
                              pygame.Rect(pos[0], pos[1], self.size, self.size))
         for block in self.body[1:]:
             if self.head[0] == block[0] and self.head[1] == block[1]:
                 game_over_window(score, game_window)
+
+    def crawl(self):
+        self.body.pop()
+
+
+class EnemySnake():
+    def __init__(self, pos_start, cell_size, speed):
+        self.size = cell_size
+        self.head = pos_start
+        pos_start = copy.deepcopy(pos_start)
+        self.body = [pos_start,
+                     [pos_start[0]+(self.size*1), pos_start[1]],
+                     [pos_start[0]+(self.size*2), pos_start[1]],
+                     [pos_start[0]+(self.size*3), pos_start[1]]
+                     ]
+        self.speed = speed
+        self.direction = "LEFT"
+        self.change_dir = self.direction
+
+    def move(self, fruit_pos, game_window):
+        self.change_dir = random_direction(self.head, fruit_pos)
+        print("1", self.change_dir)
+        self.head = pos_add(direction_check(
+            self.change_dir, self.direction), self.head, self.size)
+
+        self.body.insert(0, list(self.head))
+
+        for pos in self.body:
+            pygame.draw.rect(game_window, (255, 255, 0),
+                             pygame.Rect(pos[0], pos[1], self.size, self.size))
 
     def crawl(self):
         self.body.pop()
@@ -89,9 +120,25 @@ class Fruit():
         pygame.draw.rect(self.window, (255, 255, 255), pygame.Rect(
             self.pos[0], self.pos[1], self.size, self.size))
 
-    def redraw(self):
+    def redraw(self, placed):
         self.pos = random_pos(self.size)
+        while self.pos in placed:
+            self.pos = random_pos(self.size)
         self.draw()
+
+
+class barricade():
+    def __init__(self, game_window, cell_size, position):
+        self.locations_of = []
+        self.window = game_window
+        self.size = cell_size
+        self.pos = position
+
+    def draw(self):
+        for pos in self.pos:
+            pygame.draw.rect(self.window, (255, 50, 0), pygame.Rect(
+                pos[0], pos[1], self.size, self.size))
+            self.locations_of.append(pos)
 
 
 def game_over_window(score, game_window):
@@ -105,39 +152,112 @@ def game_over_window(score, game_window):
     game_window.blit(game_over_surface, game_over_rect)
     pygame.display.flip()
     menu_main()
+# --------------
+
+# ОСНОВНОЙ ЦИКЛ:
 
 
 def main(event, root):
 
-    global score, window_x, window_y, sell_size
+    global score, window_x, window_y, cell_size
     root.destroy()
     game_window = pygame.display.set_mode((window_x, window_y))
     is_game_over = False
     pygame.mixer.music.play(loops=0, start=0.0,
                             fade_ms=0)
     fps = pygame.time.Clock()
-    s = Snake([100, 50], sell_size, 15)
-    f = Fruit(game_window, sell_size)
+    s = Snake([100, 50], cell_size, 15)
+    f = Fruit(game_window, cell_size)
+    enemy1 = EnemySnake([200, 300], cell_size, 14)
+    block = barricade(game_window, cell_size, [[50, 55], [50, 60], [50, 65]])
 
     while not is_game_over:
+        game_window.fill((0, 0, 0))
+        enemy1.move(f.pos, game_window)
         s.move(game_window)
+
+        block.draw()
         f.draw()
         if s.head[0] == f.pos[0] and s.head[1] == f.pos[1]:
             s_hrum.play()
             score += 10
-            f.redraw()
+            f.redraw(s.body+enemy1.body+block.locations_of)
         else:
             s.crawl()
-
-        if s.head[0] < 0 or s.head[0] > window_x-sell_size:
+        if enemy1.head[0] == f.pos[0] and enemy1.head[1] == f.pos[1]:
+            f.redraw(s.body+enemy1.body+block.locations_of)
+        else:
+            enemy1.crawl()
+        if s.head[0] < 0 or s.head[0] > window_x-cell_size:
             sound_bum.play()
             game_over_window(score, game_window)
-        if s.head[1] < 0 or s.head[1] > window_y-sell_size:
+        if s.head[1] < 0 or s.head[1] > window_y-cell_size:
             sound_bum.play()
             game_over_window(score, game_window)
 
         pygame.display.update()
         fps.tick(s.speed)
+
+# Программные функции!:
+
+
+def random_direction(head, fruit):
+    i = random.randint(1, 4)
+    direction = ""
+
+    if (i == 1) or (abs(head[0]-fruit[0]) < 6) or (abs(head[1]-fruit[1]) < 6):
+        if head[0] < fruit[0]:
+            direction = "RIGHT"
+        if head[0] > fruit[0]:
+            direction = "LEFT"
+        if head[1] < fruit[1]:
+            direction = "DOWN"
+        if head[1] > fruit[1]:
+            direction = "UP"
+
+    else:
+        if head[1] < fruit[1]:
+            direction = "DOWN"
+        if head[1] > fruit[1]:
+            direction = "UP"
+        if head[0] < fruit[0]:
+            direction = "RIGHT"
+        if head[0] > fruit[0]:
+            direction = "LEFT"
+    return direction
+
+
+def direction_check(change, dir):
+    if change == 'UP' and dir != 'DOWN':
+        dir = 'UP'
+    if change == 'DOWN' and dir != 'UP':
+        dir = 'DOWN'
+    if change == 'LEFT' and dir != 'RIGHT':
+        print("LEFT")
+        dir = 'LEFT'
+    if change == 'RIGHT' and dir != 'LEFT':
+        print("RIGHT")
+        dir = 'RIGHT'
+
+    output = [0, 0, 0, 0]
+    if dir == "UP":
+        output[0] = 1
+    if dir == "DOWN":
+        output[1] = 1
+    if dir == "LEFT":
+        output[2] = 1
+    if dir == "RIGHT":
+        output[3] = 1
+    return output
+
+
+def pos_add(coef, head, size):
+    head = copy.deepcopy(head)
+    head[1] -= size*coef[0]
+    head[1] += size*coef[1]
+    head[0] -= size*coef[2]
+    head[0] += size*coef[3]
+    return head
 
 
 def random_pos(size):
@@ -145,8 +265,17 @@ def random_pos(size):
             random.randrange(1, (window_y//size)) * size]
 
 
+def on_closing():
+    pygame.quit()
+    quit()
+# --------------
+
+# ФУНКЦИИ МЕНЮ:
+
+
 def menu_main():
     root = Tk()
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     w = root.winfo_screenwidth()
     h = root.winfo_screenheight()
     w = (w//2)-200
@@ -224,11 +353,12 @@ def menu_rules(event, w, h, bg):
     but_escp = Button(ruler, text="Back", height=1, width=5)
     but_escp.bind("<Button-1>", lambda e, pr=ruler: pr.destroy())
     but_ecp_canv = canvas3.create_window(10, 10, anchor="nw", window=but_escp)
+# -------------------------
 
 
 window_x = 720
 window_y = 480
 score = 0
-sell_size = 5
+cell_size = 5
 
 menu_main()
